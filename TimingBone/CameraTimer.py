2f5eb7d3fd89083b863fc7c3a9d86ab25f58f673
Nik Karpinsky <karpinsn@gmail.com>
2012-08-23 15:54:32 -0500

    Fixed incorrect pin numbering

--- TimingBone/CameraTimer.py
+++ TimingBone/CameraTimer.py
@@ -1,38 +1,38 @@
 from time import sleep
 
 def setup():
-    # Setup pins 23 and 25 for GPIO
+    # Setup pins 23 and 25 on Expansion header B for GPIO (49 & 117)
     fileHandle = file("/sys/class/gpio/export", "w")
-    fileHandle.write("%d" % (23))
-    fileHandle.write("%d" % (25))
+    fileHandle.write("%d" % (49))
+    fileHandle.write("%d" % (117))
     fileHandle.close()
 
     # Setup pin 23 as the camera output
-    fileHandle = file("/sys/class/gpio/gpio23/direction", "w")
+    fileHandle = file("/sys/class/gpio/gpio49/direction", "w")
     fileHandle.write("out")
     fileHandle.close()
 
     # Setup pin 25 as the timing input (VSYNC in) using interrupts
-    fileHandle = file("/sys/class/gpio/gpio25/direction", "w")
+    fileHandle = file("/sys/class/gpio/gpio117/direction", "w")
     fileHandle.write("in")
     fileHandle.close()
-    fileHandle = file("/sys/class/gpio/gpio25/event", "w")
+    fileHandle = file("/sys/class/gpio/gpio117/event", "w")
     fileHandle.write("rising")
     fileHandle.close()
 
 def cleanup():
     # Cleans up the pins we were using and unexports them so others may use them
     fileHandle = file("/sys/class/gpio/unexport", "w")
-    fileHandle.write("%d" % (23))
-    fileHandle.write("%d" % (25))
+    fileHandle.write("%d" % (49))
+    fileHandle.write("%d" % (117))
 
 def generateTimingSignal():
     # Blocking read on pin 25. Will return once we have a rising edge 
-    fileHandle = file("/sys/class/gpio/gpio25/value", "r")
+    fileHandle = file("/sys/class/gpio/gpio117/value", "r")
     fileHandle.close()
 
     # Generate our timing signal
-    fileHandle = file("/sys/class/gpio/gpio23/value", "w")
+    fileHandle = file("/sys/class/gpio/gpio49/value", "w")
     fileHandle.write("1")
     fileHandle.flush()
     fileHandle.write("0")

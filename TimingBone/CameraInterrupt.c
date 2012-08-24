39e92674a8d7bceba0e2c76d4e8a28611f49f7a7
Nik Karpinsky <karpinsn@gmail.com>
2012-08-24 13:16:24 -0500

    Should be toggling based off the interrupt

--- TimingBone/CameraInterrupt.c
+++ TimingBone/CameraInterrupt.c
@@ -6,35 +6,224 @@
 #include <poll.h>
 #include <stdio.h>
 
+ /****************************************************************
+ * Constants
+ ****************************************************************/
+ 
+#define SYSFS_GPIO_DIR "/sys/class/gpio"
+#define POLL_TIMEOUT (3 * 1000) /* 3 seconds */
+#define MAX_BUF 64
+
+/****************************************************************
+ * gpio_export
+ ****************************************************************/
+int gpio_export(unsigned int gpio)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+ 
+	fd = open(SYSFS_GPIO_DIR "/export", O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/export");
+		return fd;
+	}
+ 
+	len = snprintf(buf, sizeof(buf), "%d", gpio);
+	write(fd, buf, len);
+	close(fd);
+ 
+	return 0;
+}
+
+/****************************************************************
+ * gpio_unexport
+ ****************************************************************/
+int gpio_unexport(unsigned int gpio)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+ 
+	fd = open(SYSFS_GPIO_DIR "/unexport", O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/export");
+		return fd;
+	}
+ 
+	len = snprintf(buf, sizeof(buf), "%d", gpio);
+	write(fd, buf, len);
+	close(fd);
+	return 0;
+}
+
+/****************************************************************
+ * gpio_set_dir
+ ****************************************************************/
+int gpio_set_dir(unsigned int gpio, unsigned int out_flag)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+ 
+	len = snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR  "/gpio%d/direction", gpio);
+ 
+	fd = open(buf, O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/direction");
+		return fd;
+	}
+ 
+	if (out_flag)
+		write(fd, "out", 4);
+	else
+		write(fd, "in", 3);
+ 
+	close(fd);
+	return 0;
+}
+
+/****************************************************************
+ * gpio_set_value
+ ****************************************************************/
+int gpio_set_value(unsigned int gpio, unsigned int value)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+ 
+	len = snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
+ 
+	fd = open(buf, O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/set-value");
+		return fd;
+	}
+ 
+	if (value)
+		write(fd, "1", 2);
+	else
+		write(fd, "0", 2);
+ 
+	close(fd);
+	return 0;
+}
+
+/****************************************************************
+ * gpio_get_value
+ ****************************************************************/
+int gpio_get_value(unsigned int gpio, unsigned int *value)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+	char ch;
+
+	len = snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
+ 
+	fd = open(buf, O_RDONLY);
+	if (fd < 0) {
+		perror("gpio/get-value");
+		return fd;
+	}
+ 
+	read(fd, &ch, 1);
+
+	if (ch != '0') {
+		*value = 1;
+	} else {
+		*value = 0;
+	}
+ 
+	close(fd);
+	return 0;
+}
+
+
+/****************************************************************
+ * gpio_set_edge
+ ****************************************************************/
+
+int gpio_set_edge(unsigned int gpio, char *edge)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+
+	len = snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/edge", gpio);
+ 
+	fd = open(buf, O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/set-edge");
+		return fd;
+	}
+ 
+	write(fd, edge, strlen(edge) + 1); 
+	close(fd);
+	return 0;
+}
+
+/****************************************************************
+ * gpio_fd_open
+ ****************************************************************/
+int gpio_fd_open(unsigned int gpio)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+
+	len = snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
+ 
+	fd = open(buf, O_RDONLY | O_NONBLOCK );
+	if (fd < 0) {
+		perror("gpio/fd_open");
+	}
+	return fd;
+}
+
+/****************************************************************
+ * gpio_fd_close
+ ****************************************************************/
+int gpio_fd_close(int fd)
+{
+	return close(fd);
+}
+
+void gpio_impulse(unsigned int gpio)
+{
+    gpio_set_value(gpio, 1);
+    gpio_set_value(gpio, 0);
+}
+
 int main() 
 {
-    struct pollfd pfd[2];
+    struct pollfd pfd;
 
-    int gpio_fd = open("/sys/class/gpio/gpio32/value", O_RDONLY);
+    //  Set GPIO1_4 (36) as output.
+    gpio_export(36);
+    gpio_set_dir(36, 1);
+
+    //  Setup GPIO1_0 (32) as input. We will poll the interrupt using sysfs
+    gpio_export(32);
+    gpio_set_dir(32, 0);
+    gpio_set_edge(32, "rising");
+    int gpio_fd = gpio_fd_open(32);
 
     while (1) 
     {
-	memset((void*)pfd, 0, sizeof(pfd));	
+	    memset((void*)pfd, 0, sizeof(pfd));	
 
-    	pfd[0].fd = STDIN_FILENO;
-    	pfd[0].events = POLLIN;
+    	pfd.fd = gpio_fd;
+    	pfd.events = POLLPRI;
 
-    	pfd[1].fd = gpio_fd;
-    	pfd[1].events = POLLPRI;
-
-        printf("Listening for Interrupts ...\n");
-        int ready = poll(pfd, 2, -1);
+        int ready = poll(&pfd, 2, -1);
        
-        if (ready > 0 && pfd[1].revents & POLLPRI) 
+        if (ready > 0 && pfd.revents & POLLPRI) 
         {
-            printf("Interrupt \n");
-	    char *buf[200];
-	   
-	    // Have to read in the interrupt or it will fire again
-	    int len = read(pfd[1].fd, buf, 200);
-        }
+	        char *buf[200];
+	        // Have to read in the interrupt or it will fire again
+	        int len = read(pfd.fd, buf, 200);
 
-	fflush(stdout);
+            //  Now generate our timing signal
+            gpio_impulse(36);
+            usleep(1);
+            gpio_impulse(36);
+            usleep(1);
+            gpio_impulse(36);
+        }
     }
 
     return close(gpio_fd);

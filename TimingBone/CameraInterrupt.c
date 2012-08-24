a43fdcfaed2707349bd77920c64ff57e610e2aa7
Demo User <ubuntu@omap.(none)>
2012-08-24 12:39:58 -0500

    Got Interrupts working

--- TimingBone/CameraInterrupt.c
+++ TimingBone/CameraInterrupt.c
@@ -1,25 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <poll.h>
 #include <stdio.h>
 
-void main() 
+int main() 
 {
-    struct pollfd pfd;
+    struct pollfd pfd[2];
 
-    pfd.fd = open("/sys/class/gpio/gpio70/value", O_RDONLY);
-    pfd.events = POLLPRI;
-    pfd.revents = 0;
+    int gpio_fd = open("/sys/class/gpio/gpio32/value", O_RDONLY);
 
     while (1) 
     {
-        printf("ready: %d\n", ready);
-        int ready = poll(pfd, 1, -1);
+	memset((void*)pfd, 0, sizeof(pfd));	
+
+    	pfd[0].fd = STDIN_FILENO;
+    	pfd[0].events = POLLIN;
+
+    	pfd[1].fd = gpio_fd;
+    	pfd[1].events = POLLPRI;
+
+        printf("Listening for Interrupts ...\n");
+        int ready = poll(pfd, 2, -1);
        
-        if (pfd[A].revents != 0) 
+        if (ready > 0 && pfd[1].revents & POLLPRI) 
         {
-            printf("\t Interrupt \n");
+            printf("Interrupt \n");
+	    char *buf[200];
+	   
+	    // Have to read in the interrupt or it will fire again
+	    int len = read(pfd[1].fd, buf, 200);
         }
+
+	fflush(stdout);
     }
+
+    return close(gpio_fd);
 }
 

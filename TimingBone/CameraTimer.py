f7105a75f6bbce8d134937dd4f64bd48c345d15f
Nik Karpinsky <karpinsn@gmail.com>
2012-08-23 19:30:07 -0500

    Had expansion headers backwards

--- TimingBone/CameraTimer.py
+++ TimingBone/CameraTimer.py
@@ -3,23 +3,23 @@ from bbio import *
  
 def setup():    
     print "Initializing GPIO1_17 as camera trigger output"
-    pinMode(GPIO1_17, OUTPUT)
+    pinMode(GPIO1_4, OUTPUT)
 
     print "Initializing GPIO3_21 as VSYNC timing input"
-    pinMode(GPIO3_21, INPUT)
+    pinMode(GPIO1_0, INPUT)
  
 def loop():
-    if(HIGH == digitalRead(GPIO3_21)):
-        digitalWrite(GPIO1_17, HIGH)
-        digitalWrite(GPIO1_17, LOW)
+    if(HIGH == digitalRead(GPIO1_0)):
+        digitalWrite(GPIO1_4, HIGH)
+        digitalWrite(GPIO1_4, LOW)
 
         sleep(.01)
-        digitalWrite(GPIO1_17, HIGH)
-        digitalWrite(GPIO1_17, LOW)
+        digitalWrite(GPIO1_4, HIGH)
+        digitalWrite(GPIO1_4, LOW)
 
         sleep(.01)
-        digitalWrite(GPIO1_17, HIGH)
-        digitalWrite(GPIO1_17, LOW)
+        digitalWrite(GPIO1_4, HIGH)
+        digitalWrite(GPIO1_4, LOW)
     sleep(.01) 
 
 if __name__ == '__main__':
